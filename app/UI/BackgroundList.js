a2204c1098fd6d641ec53f627f83d190fc6cbf64
Dominik <dominik.jojczyk@gmail.com>
2025-04-14 19:50:48 +0200

    11

--- app/UI/BackgroundList.js
+++ app/UI/BackgroundList.js
@@ -11,7 +11,7 @@ export default function BackgroundList({
       className={`inline-block text-center  ${bgColor} rounded-2xl p-4 py-10 md:p-10 mx-auto ${className}`}
     >
       <h3
-        className={`text-3xl mb-8 md:mb-12 font-medium leading-snug ${textColor}`}
+        className={`text-2xl mb-8 md:mb-12 font-medium leading-snug ${textColor}`}
       >
         {title}
       </h3>
--- app/UI/Header2.js
+++ app/UI/Header2.js
@@ -1,6 +1,6 @@
 export default function Header2() {
   return (
-    <h3 className="text-2xl text-center mb-10 font-bold text-customBlue">
+    <h3 className="text-3xl md:text-3xl text-center mb-10 font-bold text-customBlue">
       Cennik
     </h3>
   );
