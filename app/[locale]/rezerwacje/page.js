361ac1de2c4a70e9cbe0d9b6062e8f0fc9f7ecb6
Dominik <dominik.jojczyk@gmail.com>
2025-04-15 12:21:01 +0200

    3

--- app/[locale]/rezerwacje/page.js
+++ app/[locale]/rezerwacje/page.js
@@ -2,14 +2,16 @@ import Button from "@/app/UI/Buttons/Button";
 import Header from "@/app/UI/Header";
 import { FaPhoneAlt } from "react-icons/fa";
 import Link from "next/link";
+import Script from "next/script"; // <-- to dodaj!
 import { useTranslations } from "next-intl";
 
 export default function Rezerwacje() {
   const t = useTranslations("reservations");
+
   return (
     <>
-      <div className="">
-        <section className="">
+      <div>
+        <section>
           <Header text={t("header")} />
           <div className="px-6 md:px-20 xl:px-32 2xl:px-44 py-16 md:py-20 2xl:py-24 text-center text-xl md:w-3/4 mx-auto">
             <p className="mb-10">{t("text")}</p>
@@ -23,9 +25,30 @@ export default function Rezerwacje() {
               <Button link="/mapa-przystankow" text={t("button")} />
             </div>
           </div>
-          <div className="md:w-3/4 h-196 bg-neutral-200 mx-auto">
-            TU BEDZIE DROPLABS
+
+          {/* Początek elementu sprzedażowego Droplabs */}
+          <div className="md:w-3/4 mx-auto">
+            <iframe
+              id="dl-widget"
+              style={{
+                border: "0",
+                width: "100%",
+                padding: "0",
+                margin: "0 auto",
+                minHeight: "200px",
+                maxWidth: "1000px",
+                display: "block",
+              }}
+              src="https://widget.droplabs.pl/index.html?facilityId=222&onlineGroupId=4681"
+              title="DropLabs Rezerwacje"
+            ></iframe>
           </div>
+
+          <Script
+            src="https://widget.droplabs.pl/widget.js"
+            strategy="afterInteractive"
+          />
+          {/* Koniec elementu sprzedażowego Droplabs */}
         </section>
       </div>
     </>
