11f029f3d4d2b78a6b0fb0b5e3e878dd72d50965
Dominik <dominik.jojczyk@gmail.com>
2025-05-01 14:01:40 +0200

    1.2

--- app/UI/Homepage/Why.js
+++ app/UI/Homepage/Why.js
@@ -10,23 +10,28 @@ export default function Why() {
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-10 text-center ">
         {/* card1 */}
-        <div className="p-6 py-10 shadow-2xl rounded-3xl  border-4 border-[#005588]">
+        <div className="relative p-6 py-10 bg-gradient-to-br from-blue-50 to-red-50 shadow-2xl rounded-2xl">
           <h3 className="text-xl xl:text-2xl 2xl:text-3xl mb-10 font-semibold">
             {t("cards.1.header")}
           </h3>
           <p className="md:text-lg">{t("cards.1.text")}</p>
+          <div className="absolute top-0 left-0 w-2 h-full bg-red-600 rounded-l-3xl"></div>
         </div>
-        <div className="p-6 py-10 shadow-2xl rounded-3xl  border-4 border-[#005588] ">
-        <h3 className="text-xl xl:text-2xl 2xl:text-3xl mb-10 font-semibold">
-        {t("cards.2.header")}
+        {/* card 2 */}
+        <div className="relative p-6 py-10 bg-gradient-to-br from-blue-50 to-red-50 shadow-2xl rounded-2xl">
+          <h3 className="text-xl xl:text-2xl 2xl:text-3xl mb-10 font-semibold">
+            {t("cards.2.header")}
           </h3>
           <p className="md:text-lg">{t("cards.2.text")}</p>
+          <div className="absolute top-0 left-0 w-2 h-full bg-red-600 rounded-l-3xl"></div>
         </div>
-        <div className="p-6 py-10 shadow-2xl rounded-3xl  border-4 border-[#005588]">
-        <h3 className="text-xl xl:text-2xl 2xl:text-3xl mb-10 font-semibold">
-        {t("cards.3.header")}
+        {/* card 3 */}
+        <div className="relative p-6 py-10 bg-gradient-to-br from-blue-50 to-red-50 shadow-2xl rounded-2xl">
+          <h3 className="text-xl xl:text-2xl 2xl:text-3xl mb-10 font-semibold">
+            {t("cards.3.header")}
           </h3>
           <p className="md:text-lg">{t("cards.3.text")}</p>
+          <div className="absolute top-0 left-0 w-2 h-full bg-red-600 rounded-l-3xl"></div>
         </div>
       </div>
     </section>
