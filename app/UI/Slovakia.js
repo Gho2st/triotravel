51553cdfaf96316f6ba2770a04ae21b1857f8be3
Dominik <dominik.jojczyk@gmail.com>
2025-05-04 17:24:27 +0200

    2

---
+++ app/UI/Slovakia.js
@@ -0,0 +1,21 @@
+import { useTranslations } from "next-intl";
+
+export default function Slovakia() {
+  const t = useTranslations("slovakia");
+  return (
+    <div className="flex justify-center items-center my-16">
+      <div className="flex items-center gap-5 p-5 bg-gray-100 rounded-lg max-w-md">
+        {/* Mapa Słowacji z pliku SVG */}
+        <img src="/slovakia.svg" alt={t("alt")} className="w-24 h-auto" />
+
+        {/* Tekst promocyjny */}
+        <div>
+          <h3 className="text-2xl font-bold text-customBlue m-0">
+            {t("header")}
+          </h3>
+          <p className="text-base text-gray-700 mt-1">{t("text")}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/[locale]/wycieczki/slowacki-raj/page.js
+++ app/[locale]/wycieczki/slowacki-raj/page.js
@@ -9,6 +9,7 @@ import ButtonComponent from "./ButtonComponent";
 import { getTranslations } from "next-intl/server";
 import { routing } from "@/i18n/routing";
 import CtaLink from "@/app/UI/CtaLink";
+import Slovakia from "@/app/UI/Slovakia";
 
 export async function generateMetadata({ params }) {
   const resolvedParams = await params;
@@ -73,6 +74,7 @@ export default function SlowackiRaj() {
         <p className="mt-10 md:mt-16 text-center md:w-3/4 mx-auto xl:text-xl">
           {t("text")}
         </p>
+        <Slovakia />
         <div className="mt-16">
           <Table
             headers={tableHeaders}
