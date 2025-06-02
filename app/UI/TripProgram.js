bb4b131667c38d955dae04a92f8680b7285cbb52
Dominik <dominik.jojczyk@gmail.com>
2025-06-02 14:50:57 +0200

    2

--- app/UI/TripProgram.js
+++ app/UI/TripProgram.js
@@ -1,3 +1,5 @@
+import { FaCheck } from "react-icons/fa";
+
 export default function TripProgram({ items, title }) {
   return (
     <div>
@@ -6,7 +8,10 @@ export default function TripProgram({ items, title }) {
       </h3>
       <ul className={`text-lg flex flex-col gap-2 md:w-3/4 mx-auto `}>
         {items.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index} className="flex items-start gap-3">
+            <FaCheck className="text-red-600 mt-1 shrink-0" />
+            <span>{item}</span>
+          </li>
         ))}
       </ul>
     </div>
