914baf41e218b7c610735e39999657161f99ba51
Dominik <dominik.jojczyk@gmail.com>
2025-06-27 22:30:49 +0200

    Update Card.js

--- app/UI/Card.js
+++ app/UI/Card.js
@@ -6,8 +6,8 @@ export default function Card({ article }) {
   const t = useTranslations("offer");
 
   return (
-    <article className="flex flex-col 2xl:flex-row gap-6 p-6 rounded-2xl shadow-2xl w-full max-w-4xl transition-all duration-300 hover:shadow-3xl hover:scale-[1.02] h-full min-h-[450px]">
-      <div className="w-full 2xl:w-2/3 aspect-[4/3]">
+    <article className="flex flex-col gap-6 p-6 rounded-2xl shadow-2xl w-full max-w-4xl transition-all duration-300 hover:shadow-3xl hover:scale-[1.02] h-full min-h-[450px]">
+      <div className="w-full aspect-[4/3]">
         <Image
           src={article.image}
           width={500}
