7eddf6766434ce66fe176ee60a557bd79e3b7a00
Dominik <dominik.jojczyk@gmail.com>
2025-07-14 11:30:16 +0200

    Update layout.js

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -6,6 +6,7 @@ import { notFound } from "next/navigation";
 import { getMessages } from "next-intl/server";
 import ClientBody from "../UI/ClientBody";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -40,6 +41,16 @@ export default async function LocaleLayout({ children, params }) {
         <NextIntlClientProvider messages={messages}>
           <ClientBody fontClassName={font.className}>{children}</ClientBody>
         </NextIntlClientProvider>
+        {/* Microsoft Clarity Script */}
+        <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "se6dbhfcmd");
+          `}
+        </Script>
       </body>
     </html>
   );
