7498c5da83e9b02f5027059531e2c80cb7af1b1c
Dominik <dominik.jojczyk@gmail.com>
2025-08-10 16:32:59 +0200

    Update Header.js

--- app/UI/Header.js
+++ app/UI/Header.js
@@ -1,7 +1,9 @@
 export default function Header({ text }) {
   return (
-    <div className="bg-customRed px-6 md:px-24 py-8 md:py-12 2xl:py-14">
-      <h1 className="text-4xl font-medium leading-snug md:text-5xl md:leading-snug 2xl:text-6xl 2xl:leading-snug text-white">{text}</h1>
+    <div className="bg-customRed px-6 md:px-24 py-8 md:py-10 2xl:py-14">
+      <h1 className="text-3xl sm:text-4xl font-medium leading-snug  md:leading-snug 2xl:text-6xl 2xl:leading-snug text-white">
+        {text}
+      </h1>
     </div>
   );
 }
